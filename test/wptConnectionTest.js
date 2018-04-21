939a737f572034e66042a7bdddafc57b950cc52a
Tobias Lidskog <tobias@lidskog.se>
2018-04-21 17:43:11 +0200

    Mark async methods as such.
    The async keywork is helpful when reading code, to see what’s returning a Promise.

--- test/wptConnectionTest.js
+++ test/wptConnectionTest.js
@@ -11,28 +11,32 @@ const pipeTo = require('./util').pipeTo;
 
 const connection = WptConnection.toPublicHost(process.env.WPT_KEY);
 
-connection
-  .getTesters()
-  .then(() => {
-    const testId = '170827_PK_4ddfa1b1d107f436f5be5178ec320147';
+async function runTest(connection) {
+  await connection.getTesters();
 
-    const videoSpec = new VideoSpecBuilder()
-      .withTestId(testId)
-      .withRun(2)
-      .withLabel('foo')
-      .build();
-    const video = connection
-      .createVideo(videoSpec)
-      .then(video => video.videoId)
-      .then(videoId => connection.getVideo(videoId))
-      .then(stream => pipeTo(stream, fs.createWriteStream('/tmp/video.mp4')));
-    const har = connection
-      .getHARData(testId)
-      .then(stream => pipeTo(stream, process.stdout));
-    const testResults = connection
-      .getTestResult(testId)
-      .then(r => console.log(JSON.stringify(r)));
+  const testId = '170827_PK_4ddfa1b1d107f436f5be5178ec320147';
 
-    return Promise.all([video, har, testResults]);
-  })
-  .catch(e => console.error(e));
+  const videoSpec = new VideoSpecBuilder()
+    .withTestId(testId)
+    .withRun(2)
+    .withLabel('foo')
+    .build();
+
+  const video = connection
+    .createVideo(videoSpec)
+    .then(video => video.videoId)
+    .then(videoId => connection.getVideo(videoId))
+    .then(stream => pipeTo(stream, fs.createWriteStream('/tmp/video.mp4')));
+
+  const har = connection
+    .getHARData(testId)
+    .then(stream => pipeTo(stream, process.stdout));
+
+  const testResults = connection
+    .getTestResult(testId)
+    .then(r => console.log(JSON.stringify(r)));
+
+  return Promise.all([video, har, testResults]);
+}
+
+runTest(connection).catch(e => console.error(e));
